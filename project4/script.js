af1a79d82c56b3d411e9b675ff57d4179a2b1b36
Christina Williams <cmw78@duke.edu>
2017-11-19 19:28:23 -0500

    draft

---
+++ project4/script.js
@@ -0,0 +1,28 @@
+
+var baseURL = window.location.href.substring(0, window.location.href.length-10);
+
+function alertUser(e){
+	alert("you clicked this at " + e.timeStamp);
+}
+
+$(function(){
+	$('h1').on('click',function(e){
+		alertUser(e);
+	});
+
+	});
+var items = [];
+
+function newElement(){
+ var input = $('#items').val();
+ var split = input.split(',');
+ console.log(split);
+ $.post(baseURL+'output.txt', input);
+
+
+};
+
+$('#save').click(
+	function(){
+		newElement();
+	});
